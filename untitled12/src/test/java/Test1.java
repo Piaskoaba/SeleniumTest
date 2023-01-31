d1e251468e2882b6ee6b9be81cc3f8eba034679a
Piaskoba <bartosz.piaskowskii@gmail.com>
2023-01-31 19:01:13 +0100

    Dodanie 1pierwszego testu - uruchamianie przegladarki

---
+++ untitled12/src/test/java/Test1.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Test1 {
+    @Test
+    public void demoTest() {
+        WebDriver driver = new ChromeDriver();
+    }
+}
+
+
