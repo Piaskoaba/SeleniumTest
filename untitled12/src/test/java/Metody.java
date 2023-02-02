609342ac7b4a8f68ba5fd6c78caf9380d4440bbb
Piaskoba <bartosz.piaskowskii@gmail.com>
2023-02-02 17:40:03 +0100

    +Metody za chuj nie wiem dlaczego nie dziala

---
+++ untitled12/src/test/java/Metody.java
@@ -0,0 +1,22 @@
+import org.junit.jupiter.api.Test;
+
+public class Metody {
+
+    @Test
+    public void examples() {
+        double poleTrapezuPierwszego = obliczPole( adam 8, ewa 10, kuba 5); //chuj wie, nie dziala
+        double poleTrapezuDrugiego = ((6+8)*3/2);
+        double poleTrapezuTrzeciego = ((12+10)*6/2);
+        String tekst = " Pola trapezów to: " + poleTrapezuPierwszego + "," + poleTrapezuDrugiego + "," + poleTrapezuTrzeciego + ",";
+        System.out.print("Koniec testu");
+    }
+    double obliczPole = (double adam,double ewa,double kuba);
+    double poleTrapezu = ((adam + ewa)*kuba)/2;
+    return poleTrapezu;
+
+    }
+
+
+void wypiszKoniec(){
+    System.out.print();
+}
--- untitled12/src/test/java/Test1.java
+++ untitled12/src/test/java/Test1.java
@@ -45,4 +45,7 @@ public class Test1 {
 
 
 
-}
+
+    }
+
+
