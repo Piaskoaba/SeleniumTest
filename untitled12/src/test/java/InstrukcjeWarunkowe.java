0efe4551393dff780a1c7216185a6ae5d2041a38
Piaskoba <bartosz.piaskowskii@gmail.com>
2023-02-03 14:26:18 +0100

    p

--- untitled12/src/test/java/InstrukcjeWarunkowe.java
+++ untitled12/src/test/java/InstrukcjeWarunkowe.java
@@ -3,16 +3,16 @@ import org.junit.jupiter.api.Test;
 public class InstrukcjeWarunkowe {
     @Test
     public void ifexample() {
-        int liczba = 99;
+        int liczba = 100;
 
 
         if (liczba > 100) {                         //jesli
             System.out.println("Liczba " + liczba + " Jest wieksza od 100");
-
+        liczba++;                   //powieksz o 1
         }
         else if (0<=liczba && liczba <=100){    // w przeciwnym razie, jesli // 0<= i <=100
             System.out.println("Wartosc" + liczba + "Jest mniejsza  lub rowna 100 ");
-            if(liczba==99) System.out.println("Wygrales roll! Item for U");
+            if(liczba==100) System.out.println("Wygrales roll! Item for U");
             liczba = 100;
         }
                                 //jezeli nie spelni warunkow wyzej, przechodzi do else
@@ -26,7 +26,7 @@ public class InstrukcjeWarunkowe {
     @Test
 
     public void switchExample() {
-        int liczba = 2;
+        int liczba = 43;
 
         switch (liczba) {
             case 0:                //przypadek  //punkt wejscia, spelnil warunek
@@ -37,6 +37,9 @@ public class InstrukcjeWarunkowe {
             break;
             case 2:
             System.out.println(" Liczba jest rowna 2" );
+            break;
+            case 43:
+                System.out.println( " Liczba jest równa 43");
             break;
             default:        // jezeli warunki nie sa spełnione
             System.out.println(" Liczba jest nieprawidłowa ");
