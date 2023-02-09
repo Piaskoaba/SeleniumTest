d4152e2e1f1c082aeff865600da28e77ddfdbda6
Piaskoba <bartosz.piaskowskii@gmail.com>
2023-02-09 11:43:45 +0100

    SUPER

---
+++ untitled12/src/test/java/Super/AstronomicalObject.java
@@ -0,0 +1,29 @@
+package Super;
+
+public class AstronomicalObject {
+    private int orbitalPeriodInDays;
+    private float radius;
+    private String name;
+
+    public AstronomicalObject(String name) {
+        this.name = name;
+
+    }
+
+    public AstronomicalObject(String name, int orbitalPeriodInDays, float radius) {
+        this.name;
+        this.orbitalPeriodInDays;
+        this.radius;
+
+    }
+
+    public AstronomicalObject() {
+
+    }
+
+    public int getOrbitalPeriod() {
+        return orbitalPeriodInDays;
+    }
+
+}
+
---
+++ untitled12/src/test/java/Super/PlanetTest.java
@@ -0,0 +1,11 @@
+package Super;
+
+import org.junit.jupiter.api.Test;
+
+public class PlanetTest {
+@Test
+public void test() {
+    Planet earth = new Planet("earth");
+    Planet mars = new Planet("Mars", 687, 3389.5f, false);
+}
+}
