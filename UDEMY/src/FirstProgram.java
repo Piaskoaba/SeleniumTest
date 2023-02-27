93778550911e361e022d084ead2ee7c41a25500d
Piaskoba <bartosz.piaskowskii@gmail.com>
2023-02-27 14:24:50 +0100

    UDEMY, kurs

---
+++ UDEMY/src/FirstProgram.java
@@ -0,0 +1,22 @@
+public class FirstProgram {
+
+    public static void main (String[] args) {
+
+        System.out.println(" Kurs UDEMY");
+        int prize;
+        prize = 40;
+        System.out.println(prize);
+        double charge = 12.99;
+        String tekst = (" is not ");
+
+        System.out.println(charge + tekst + prize );
+
+    }
+    }
+
+
+
+
+
+
+
